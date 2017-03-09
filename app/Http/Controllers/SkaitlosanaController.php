7778e4b537db170964cd69c6d68cb17e16e7dbb1
Mārcis <akonens@inbox.lv>
2017-03-09 21:39:15 +0200

    Pievienoti saraksti

--- app/Http/Controllers/SkaitlosanaController.php
+++ app/Http/Controllers/SkaitlosanaController.php
@@ -3,11 +3,18 @@ namespace App\Http\Controllers;
 
 class SkaitlosanaController {
   public function index() {
+    $name='PDGEsdsRWEsS';
     $numbers=[1,3,2,4,2,3,5,3];
     $data['list']=$numbers;
     sort ($numbers);
     $data['sorted_list']=$numbers;
-    
+    $data['bigest']=max ($numbers);
+    $data['small']=min ($numbers);
+    $data['count']=count ($numbers);
+    $data['name']=$name;
+    $data['strlen']=strlen ($name);
+    $data['name']=strtolower ($name);
+
     return view('skaitlosana',$data);
 
   }
--- resources/views/skaitlosana.blade.php
+++ resources/views/skaitlosana.blade.php
@@ -3,10 +3,23 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+
     <h1>Nesakārtots saraksts</h1>
       {{ json_encode ($list) }}
     <h1>Sakārtots saraksts</h1>
       {{ json_encode ($sorted_list) }}
+    <h1>Lielākais skaitlis</h1>
+      {{ json_encode ($bigest) }}
+    <h1>Mazākais cipars</h1>
+      {{ json_encode ($small) }}
+    <h1>Cik ir vienibas</h1>
+      {{ json_encode ($count) }}
+    <h1>Name</h1>
+      {{ $name }}
+    <h2>Cik burti vardam</h2>
+      {{ $strlen }}
+    <h2>Izdara lai burti ir mazi</h2>
+      {{ $name }}
   </head>
   <body>
 
